dadac47c34b55b871180bc837a9aaf82e97c7512
vikas <vicky582050@gmail.com>
2024-03-20 22:36:48 +0530

    my jwt commit

--- backend/src/Routes/routes.ts
+++ backend/src/Routes/routes.ts
@@ -1,23 +1,64 @@
-import  {Hono}  from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
-const api = new Hono().basePath('/api/v1')
+const api = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_secret: string;
+  };
+}>().basePath("/v1");
 
+api.post("/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
 
-api.post('/user/signup', (c) => {
-    return c.text('Hello Hono!')
-  })     
-  api.post('/user/signin', (c) => {
-    return c.text('Hello Hono!')
-  })   
-  api.post('/blog', (c) => {
-    return c.text('Hello Hono!')
-  })   
-  api.put('/blog', (c) => {
-    return c.text('Hello Hono!')
-  })   
-  api.get('/blog/:id', (c) => {
-    return c.text('Hello Hono!')
-  })  
-  api.get('/blog/bulk', (c) => {
-    return c.text('Hello Hono!')
-  })    
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+  const payload = {
+    id: user.id,
+  };
+  const token = await sign(payload, c.env.JWT_secret);
+  return c.json({
+    jwt: token,
+  });
+});
+api.post("/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+  const jwt = await sign({ id: user?.id }, c.env.JWT_secret);
+  return c.json({
+    jwt,
+  });
+});
+
+api.post("/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+api.put("/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+api.get("/blog/:id", (c) => {
+  return c.text("Hello Hono!");
+});
+api.get("/blog/bulk", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default api;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,33 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import api from "./Routes/routes";
+import { verify } from "hono/jwt";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+app.route("/api", api);
+
+app.use("/api/v1/blog/*", async (c, next) => {
+  const jwt = c.req.header("authorization");
+  if (!jwt) {
+    c.status(400);
+    return c.json({ error: "unauthorized" });
+  }
+  const token = jwt.split(" ")[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  c.set('userId',payload.id);
+  await next()
+});
 
 export default app;
